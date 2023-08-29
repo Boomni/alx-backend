42978998059224474ec9a1268ce82184eb7b7b3a
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-08-29 17:56:01 +0100

    Basic Babel setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 """Simple flask app to practice i18n"""
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__)
+class Config():
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+app.config.from_object(Config)
+
+babel = Babel(app)
 
 @app.route('/')
 def index():
