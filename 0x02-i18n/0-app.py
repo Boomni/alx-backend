60fb518aa941aeacb450415be9d5e2d503a549bb
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-08-29 04:09:38 -0700

    Simple flask template

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Simple flask app to practice i18n"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Renders a simple webpage"""
+    render_template("0-index.html")
+
+if __name__ == "__main__":
+    app.run()
