3f22f8ee4e4aab892bae98d23b51fd1a650dbe4b
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-08-30 15:23:03 +0100

    Implemented Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Simple flask app to practice i18n"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
+import gettext
 from flask_babel import Babel
 
-
 app = Flask(__name__)
-
+app.secret_key = '32b3giuej as57r6t'
 
 class Config():
     """Class to configure available languages in our app"""
@@ -19,16 +19,40 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route('/')
-def index() -> str:
-    """Renders a simple webpage"""
-    return render_template("3-index.html")
-
-
 @babel.localeselector
 def get_locale():
     """Get locale from request"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """"""
+    login_as = request.args.get('login_as')
+    if login_as:
+        return users.get(int(login_as))
+    return None
+
+
+@app.before_request
+def before_request():
+    g.user = get_user()
+
+
+@app.route('/')
+def index():
+    """Renders a simple webpage"""
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
