627ca2377c21ba005dbca949504b239e5f8507c7
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-08-30 11:28:09 +0100

    Implemented [fr|en] languages support

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Simple flask app to practice i18n"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -20,20 +20,17 @@ babel = Babel(app)
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """Renders a simple webpage"""
-    home_title: str = _('Welcome to Holberton')
-    home_header: str = _('Hello world!')
-    return render_template(
-            "3-index.html",
-            home_title=home_title,
-            home_header=home_header
-            )
+    return render_template("4-index.html")
 
 
 @babel.localeselector
 def get_locale():
     """Get locale from request"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Simple flask app to practice i18n"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -20,15 +20,9 @@ babel = Babel(app)
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """Renders a simple webpage"""
-    home_title: str = _('Welcome to Holberton')
-    home_header: str = _('Hello world!')
-    return render_template(
-            "3-index.html",
-            home_title=home_title,
-            home_header=home_header
-            )
+    return render_template("3-index.html")
 
 
 @babel.localeselector
